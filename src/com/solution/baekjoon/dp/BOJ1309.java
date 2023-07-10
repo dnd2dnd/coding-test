851999f2f2e21c0db2fe4317beecb16c22b1b699
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-07-10 22:07:27 +0900

    dp 너무 어려움 ./.

--- src/com/solution/baekjoon/dp/BOJ1309.java
+++ src/com/solution/baekjoon/dp/BOJ1309.java
@@ -13,45 +13,17 @@ public class BOJ1309 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
 
-		int n = Integer.parseInt(st.nextToken());
-		int[][] dp=new int[n+1][3];
-		Arrays.fill(dp[1],1);
+		long[][] dp = new long[N+1][3];
 
-		for(int i=2;i<=n;i++){
-			dp[i][0]=dp[i-1][0]+dp[i-1][1]+dp[i-1][2];
-			dp[i][1]=dp[i-1][2]+dp[i-1][0];
-			dp[i][2]=dp[i-1][1]+dp[i-1][0];
-			dp[i][0] %= 9901;
-			dp[i][1] %= 9901;
-			dp[i][2] %= 9901;
+		dp[1][0] = dp[1][1] = dp[1][2] = 1;
+
+		for(int i=2; i<=N; i++) {
+			dp[i][0] = (dp[i-1][0]+dp[i-1][1]+dp[i-1][2]) % 9901;
+			dp[i][1] = (dp[i-1][0]+dp[i-1][2]) % 9901;
+			dp[i][2] = (dp[i-1][0]+dp[i-1][1]) % 9901;
 		}
-
-		int res=0;
-
-		for(int i =0; i<3; i++){
-			res+=dp[n][i];
-		}
-
-		System.out.println(res%9901);
-		// long[][] com = new long[n+1][n+1];
-		// com[1][0] = 1;
-		// com[1][1] = 1;
-		// int sum = 0;
-		// for(int i=2; i<=n; i++) {
-		// 	com[i][0]=1;
-		// 	for(int j=1; j<=n; j++) {
-		// 		com[i][j] = com[i-1][j-1] + com[i-1][j];
-		// 		sum += com[i][j];
-		// 	}
-		// 	sum += com[i][0];
-		// }
-		// for(int i=0; i<=n; i++) {
-		// 	for(int j=0; j<=n; j++) {
-		// 		System.out.print(com[i][j] + " ");
-		// 	}
-		// 	System.out.println();
-		// }
-		// System.out.println(sum%9901);
+		System.out.println((dp[N][0] + dp[N][1] + dp[N][2]) % 9901);
 	}
 }
