fe69c19af0c9820e6d1641b2bb6ab79474cd8a1f
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-07-14 01:09:42 +0900

    치즈 간단한 풀이

--- src/com/solution/baekjoon/dfsbfs/BOJ2636.java
+++ src/com/solution/baekjoon/dfsbfs/BOJ2636.java
@@ -19,6 +19,7 @@ public class BOJ2636 {
 	static int[] dx = {1, -1, 0, 0};
 	static int[] dy = {0, 0, 1, -1};
 	static int X, Y;
+	static int cheese;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,92 +31,81 @@ public class BOJ2636 {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<n; j++) {
 				space[i][j] = Integer.parseInt(st.nextToken());
-				if(space[i][j]==0){
-					X=i;
-					Y=j;
+				if(space[i][j]==1){
+					cheese++;
 				}
 			}
 		}
-		int cnt = 0;
-		List<Integer> list = new ArrayList<>();
-		while(true) {
-			int cheese = 0;
-			if(!airBfs(X, Y)){
-				break;
-			}
-			boolean[][] check = new boolean[m][n];
-
-			for(int i=0; i<m; i++) {
-				for(int j=0; j<n; j++) {
-					if(!check[i][j]) {
-						cheese+=cheeseDfs(i, j, check);
-					}
-				}
-			}
-			list.add(cheese);
-			// System.out.println();
-			cnt++;
+		int time=0;
+		int lastCheese =0;
+		while (cheese!=0) {
+			lastCheese = cheese;
+			bfs();
+			time++;
 		}
-		System.out.println(cnt-1);
-		System.out.println(list.get(list.size()-2));
+		System.out.println(time);
+		System.out.println(lastCheese);
+		// for(int i=0; i<m; i++) {
+		// 	st = new StringTokenizer(br.readLine());
+		// 	for(int j=0; j<n; j++) {
+		// 		space[i][j] = Integer.parseInt(st.nextToken());
+		// 		if(space[i][j]==0){
+		// 			X=i;
+		// 			Y=j;
+		// 		}
+		// 	}
+		// }
+		// int cnt = 0;
+		// List<Integer> list = new ArrayList<>();
+		// while(true) {
+		// 	int cheese = 0;
+		// 	if(!airBfs(X, Y)){
+		// 		break;
+		// 	}
+		// 	boolean[][] check = new boolean[m][n];
+		//
+		// 	for(int i=0; i<m; i++) {
+		// 		for(int j=0; j<n; j++) {
+		// 			if(!check[i][j]) {
+		// 				cheese+=cheeseDfs(i, j, check);
+		// 			}
+		// 		}
+		// 	}
+		// 	list.add(cheese);
+		// 	// System.out.println();
+		// 	cnt++;
+		// }
+		// System.out.println(cnt-1);
+		// System.out.println(list.get(list.size()-2));
 	}
 
-	public static int cheeseDfs(int x, int y, boolean[][] check) {
-		check[x][y] = true;
-		int sx, sy;
-		int cheese = 0;
-		for(int i=0; i<4; i++) {
-			sx = x + dx[i];
-			sy = y + dy[i];
-
-			if(sx<0 || sy<0 || sx>=m || sy>=n) continue;
-
-			if(space[x][y]==1 && space[sx][sy]==2) {
-				cheese++;
-				space[x][y] = 0;
-			}
-
-			// if(!check[sx][sy]) {
-			// 	check[sx][sy]=true;
-			// }
-		}
-		return cheese;
-	}
-
-	public static boolean airBfs(int x, int y) {
+	public static void bfs() {
 		Queue<Node> queue = new LinkedList<>();
-		queue.add(new Node(x, y));
+		queue.add(new Node(0, 0));
 		boolean[][] check = new boolean[m][n];
-		boolean b = false;
 		while(!queue.isEmpty()) {
 			Node node = queue.poll();
-			int sx, sy;
-			for (int i=0; i<4; i++) {
-				sx = node.x + dx[i];
-				sy = node.y + dy[i];
 
-				if(sx<0 || sy<0 || sx>=m || sy>=n) continue;
+			int nx, ny;
+			for(int i=0; i<4; i++) {
+				nx = node.x + dx[i];
+				ny = node.y + dy[i];
 
-				if(!check[sx][sy] & space[sx][sy]!=1) {
-					queue.add(new Node(sx, sy));
-					check[sx][sy]=true;
-					if(space[sx][sy]==0) {
-						space[sx][sy] = 2;
-						b = true;
+				if(nx<0 || ny<0 || nx>=m || ny>=n) continue;
+
+				if(!check[nx][ny]) {
+					if(space[nx][ny]==1) {
+						space[nx][ny] = 0;
+						cheese--;
+					} else {
+						queue.add(new Node(nx, ny));
 					}
+					check[nx][ny] = true;
 				}
 			}
 		}
-		// for(int i=0; i<m; i++) {
-		//    for(int j=0; j<n; j++) {
-		//       System.out.print(space[i][j]+" ");
-		//    }
-		//    System.out.println();
-		// }
-		// System.out.println();
-		return b;
-
 	}
+
 	public static class Node {
 		int x, y;
 
@@ -163,4 +153,62 @@ public class BOJ2636 {
 		// 	System.out.println();
 		// }
 	}
+
+	// public static int cheeseDfs(int x, int y, boolean[][] check) {
+	// 	check[x][y] = true;
+	// 	int sx, sy;
+	// 	int cheese = 0;
+	// 	for(int i=0; i<4; i++) {
+	// 		sx = x + dx[i];
+	// 		sy = y + dy[i];
+	//
+	// 		if(sx<0 || sy<0 || sx>=m || sy>=n) continue;
+	//
+	// 		if(space[x][y]==1 && space[sx][sy]==2) {
+	// 			cheese++;
+	// 			space[x][y] = 0;
+	// 		}
+	//
+	// 		// if(!check[sx][sy]) {
+	// 		// 	check[sx][sy]=true;
+	// 		// }
+	// 	}
+	// 	return cheese;
+	// }
+	//
+	// public static boolean airBfs(int x, int y) {
+	// 	Queue<Node> queue = new LinkedList<>();
+	// 	queue.add(new Node(x, y));
+	// 	boolean[][] check = new boolean[m][n];
+	// 	boolean b = false;
+	// 	while(!queue.isEmpty()) {
+	// 		Node node = queue.poll();
+	// 		int sx, sy;
+	// 		for (int i=0; i<4; i++) {
+	// 			sx = node.x + dx[i];
+	// 			sy = node.y + dy[i];
+	//
+	// 			if(sx<0 || sy<0 || sx>=m || sy>=n) continue;
+	//
+	// 			if(!check[sx][sy] & space[sx][sy]!=1) {
+	// 				queue.add(new Node(sx, sy));
+	// 				check[sx][sy]=true;
+	// 				if(space[sx][sy]==0) {
+	// 					space[sx][sy] = 2;
+	// 					b = true;
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	// for(int i=0; i<m; i++) {
+	// 	//    for(int j=0; j<n; j++) {
+	// 	//       System.out.print(space[i][j]+" ");
+	// 	//    }
+	// 	//    System.out.println();
+	// 	// }
+	// 	// System.out.println();
+	// 	return b;
+	//
+	// }
+
 }
