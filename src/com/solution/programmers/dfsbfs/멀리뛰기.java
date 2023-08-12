ff8f3ef92a374b7b04309f95a58c82221c1bb436
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-08-12 23:25:09 +0900

    멀리뛰기

---
+++ src/com/solution/programmers/dfsbfs/멀리뛰기.java
@@ -0,0 +1,20 @@
+package com.solution.programmers.dfsbfs;
+
+public class 멀리뛰기 {
+    class Solution {
+        public long solution(int n) {
+            int dp[] = new int[2001];
+            long answer = 0;
+            dp[0] = 1;
+            dp[1] = 1;
+
+            for(int i=2; i<=n; i++) {
+                // dp[i] = dp[i-1]%1234567 + dp[i-2]%1234567;
+                dp[i] = (dp[i-1] + dp[i-2])%1234567;
+            }
+
+
+            return dp[n];
+        }
+    }
+}
