0907df0e52ce219a957b3fbdb14753c1c1616c72
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-09-21 23:47:57 +0900

    백준 섬의 개수 재풀이

--- src/com/solution/baekjoon/dfsbfs/BOJ4963.java
+++ src/com/solution/baekjoon/dfsbfs/BOJ4963.java
@@ -79,3 +79,69 @@ public class BOJ4963 {
 		}
 	}
 }
+
+// import java.io.BufferedReader;
+// 	import java.io.IOException;
+// 	import java.io.InputStreamReader;
+// 	import java.util.LinkedList;
+// 	import java.util.Queue;
+// 	import java.util.StringTokenizer;
+//
+// public class Main {
+// 	public static class Node {
+// 		int x, y;
+//
+// 		public Node(int x, int y) {
+// 			this.x = x;
+// 			this.y = y;
+// 		}
+// 	}
+// 	static int w, h;
+// 	static int[] dx = {1, -1, 0, 0, 1, -1, 1, -1};
+// 	static int[] dy = {0, 0, -1, 1, -1, -1, 1, 1};
+// 	static int[][] map;
+// 	public static void main(String[] args) throws IOException {
+// 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 		while(true) {
+// 			StringTokenizer st = new StringTokenizer(br.readLine());
+// 			w = Integer.parseInt(st.nextToken());
+// 			h = Integer.parseInt(st.nextToken());
+//
+// 			if(w==0 && h==0) break;
+//
+// 			map = new int[h][w];
+//
+// 			for(int i=0; i<h; i++) {
+// 				st = new StringTokenizer(br.readLine());
+// 				for(int j=0; j<w; j++) {
+// 					map[i][j] = Integer.parseInt(st.nextToken());
+// 				}
+// 			}
+//
+// 			int sum = 0;
+// 			for(int i=0; i<h; i++) {
+// 				for(int j=0; j<w; j++) {
+// 					if(map[i][j]==1) {
+// 						map[i][j]=0;
+// 						dfs(i, j);
+// 						sum++;
+// 					}
+// 				}
+// 			}
+// 			System.out.println(sum);
+// 		}
+// 	}
+//
+// 	public static void dfs(int x, int y) {
+// 		int nx, ny;
+// 		for(int i=0; i<8; i++) {
+// 			nx = x + dx[i];
+// 			ny = y + dy[i];
+//
+// 			if(nx<0 || ny<0 || nx>=h || ny>=w || map[nx][ny]==0) continue;
+//
+// 			map[nx][ny]=0;
+// 			dfs(nx, ny);
+// 		}
+// 	}
+// }
