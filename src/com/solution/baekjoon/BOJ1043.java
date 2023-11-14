802b7595a5d6c7b5b1ae6bbbd8e72ed648cabdc2
tkfdkskarl56 <tkfdkskarl56@naver.com>
2023-11-14 23:00:18 +0900

    1043 거짓말 코드 수정 & 1976 여행가자

--- src/com/solution/baekjoon/BOJ1043.java
+++ src/com/solution/baekjoon/BOJ1043.java
@@ -6,71 +6,82 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
+/**
+ * 백준 1043, 거짓말, 골드 4
+ */
 public class BOJ1043 {
-	static int n, m;
-	static boolean[] trueCheck;
+	static int[] parent;
+	static boolean[] check;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		trueCheck = new boolean[n+1];
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		parent = new int[n+1];
+		for(int i=0; i<=n; i++) {
+			parent[i] = i;
+		}
+
+		check = new boolean[n+1];
 		st = new StringTokenizer(br.readLine());
-		int x = Integer.parseInt(st.nextToken());
-		if(x!=0) {
-			for(int i=1; i<=x; i++) {
-				int w = Integer.parseInt(st.nextToken());
-				trueCheck[w] = true;
+		int A = Integer.parseInt(st.nextToken());
+		if(A>0) {
+			for(int i=0; i<A; i++) {
+				check[Integer.parseInt(st.nextToken())] = true;
 			}
 		}
 
 		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-		for(int i=0; i<=n; i++) {
+		for(int i=0; i<m; i++) {
 			list.add(new ArrayList<>());
 		}
 
-
-		// boolean[] copyCheck;
-		for(int i=1; i<=m; i++) {
-			// copyCheck = copyCheck();
+		for(int i=0; i<m; i++) {
 			st = new StringTokenizer(br.readLine());
-			int q = Integer.parseInt(st.nextToken());
-			for(int j=1; j<=q; j++) {
-				int w = Integer.parseInt(st.nextToken());
-				list.get(q).add(w);
+			int a = Integer.parseInt(st.nextToken());
+			ArrayList<Integer> temp = new ArrayList<>();
+			for(int j=0; j<a; j++) {
+				int k = Integer.parseInt(st.nextToken());
+				list.get(i).add(k);
+				temp.add(k);
+			}
+
+			for(int j=0; j < temp.size()-1; j++) {
+				union(temp.get(j), temp.get(j+1));
+			}
+		}
+
+		for(int i=1; i<=n; i++) {
+			if(check[i]) {
+				check[find(i)] = true;
 			}
 		}
 
 		int cnt = 0;
-		for(int i=list.size()-1; i>0; i--) {
-			if(list.get(i).size() == 0) continue;
-
-			for(int j=0; j<list.get(i).size(); j+=i) {
-				boolean c = false;
-				for(int k=j; k<j+i; k++) {
-					if(trueCheck[list.get(i).get(k)]) {
-						c = true;
-						break;
-					}
-				}
-
-				if(c) {
-					for(int k=j; k<j+i; k++) {
-						trueCheck[list.get(i).get(k)] = true;
-					}
-				} else {
-					cnt++;
+		for(int i=0; i<m; i++) {
+			boolean b = false;
+			for (Integer j : list.get(i)) {
+				if(check[find(j)]) {
+					b = true;
+					break;
 				}
 			}
+			if(!b) cnt++;
 		}
+
 		System.out.println(cnt);
+	}
+	public static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+
+		if(x==y) return;
+		parent[y] = x;
 
 	}
-	public static boolean[] copyCheck() {
-		boolean[] copy = new boolean[n+1];
-		for(int i=0; i<n+1; i++) {
-			copy[i] = trueCheck[i];
-		}
-		return copy;
+	public static int find(int x) {
+		if(parent[x] == x) return x;
+		return parent[x] = find(parent[x]);
 	}
 }
--- src/com/solution/baekjoon/mst/BOJ1976.java
+++ src/com/solution/baekjoon/mst/BOJ1976.java
@@ -1,12 +1,60 @@
-package com.solution.baekjoon.mst;/**
- * description    :
- * packageName    : com.solution.baekjoon.mst
- * fileName       : BOJ1976
- * author         : tkfdk
- * date           : 2023-11-14
- * ===========================================================
- * DATE              AUTHOR             NOTE
- * -----------------------------------------------------------
- * 2023-11-14        tkfdk       최초 생성
- */public class BOJ1976 {
+package com.solution.baekjoon.mst;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 1976, 여행 가자, 골드 4
+ */
+public class BOJ1976 {
+    static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        parent = new int[n+1];
+        for(int i=1; i<=n; i++) {
+            parent[i] = i ;
+        }
+        StringTokenizer st;
+        for(int i=1; i<=n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for(int j=1; j<=n; j++) {
+                if(Integer.parseInt(st.nextToken())==1) {
+                    union(i, j);
+                }
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<m; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        String answer = "YES";
+        int check = find(list.get(0));
+        for(int i=1; i<list.size(); i++) {
+            if(check != find(list.get(i))) {
+                answer = "NO";
+                break;
+            }
+        }
+        System.out.println(answer);
+    }
+    public static int find(int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x==y) return;
+        parent[y] = x;
+    }
 }
