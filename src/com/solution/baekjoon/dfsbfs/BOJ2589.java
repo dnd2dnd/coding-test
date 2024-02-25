2347794f03880028c5ecc719e39b8cadaedb7b91
tkfdkskarl56 <tkfdkskarl56@naver.com>
2024-02-25 21:09:53 +0900

    보물섬

--- src/com/solution/baekjoon/dfsbfs/BOJ2589.java
+++ src/com/solution/baekjoon/dfsbfs/BOJ2589.java
@@ -7,77 +7,62 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-/**
- * 백준 2589, 보물섬, 골드5
- */
 public class BOJ2589 {
 	public static class Node {
-		int x, y, d;
+		int x, y, v;
 
-		public Node(int x, int y, int d) {
+		public Node(int x, int y, int v) {
 			this.x = x;
 			this.y = y;
-			this.d = d;
+			this.v = v;
 		}
 	}
+	static String[][] maps;
 	static int[] dx = {1, -1, 0, 0};
 	static int[] dy = {0, 0, 1, -1};
-	static boolean[][] visited;
-	static String[][] map;
-	static int l, w;
-	static int max = 0;
+	static int l,w;
+	static int time = Integer.MIN_VALUE;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
 		l = Integer.parseInt(st.nextToken());
 		w = Integer.parseInt(st.nextToken());
-		map = new String[l][w];
 
+		maps = new String[l][w];
 		for(int i=0; i<l; i++) {
-			String[] strs = br.readLine().split("");
-			for(int j=0; j<w; j++) {
-				map[i][j] = strs[j];
-			}
+			maps[i] = br.readLine().split("");
 		}
 
-		visited = new boolean[l][w];
-
 		for(int i=0; i<l; i++) {
 			for(int j=0; j<w; j++) {
-				if(map[i][j].equals("L") && !visited[i][j]) {
-					max = Math.max(max, bfs(i, j, 0));
-					visited = new boolean[l][w];
+				if(maps[i][j].equals("L")) {
+					bfs(i, j);
 				}
 			}
 		}
-		System.out.println(max);
+		System.out.println(time);
 	}
-	public static int bfs(int x, int y, int d) {
+	public static void bfs(int x, int y) {
+		boolean[][] visited = new boolean[l][w];
 		Queue<Node> queue = new LinkedList<>();
-		queue.add(new Node(x, y, d));
+		queue.add(new Node(x, y, 0));
 		visited[x][y] = true;
-		int answer = 0;
+
 		while(!queue.isEmpty()) {
 			Node node = queue.poll();
-			if(answer< node.d) {
-				answer = node.d;
-			}
+			time = Math.max(time, node.v);
 			int nx, ny;
 			for(int i=0; i<4; i++) {
 				nx = node.x + dx[i];
 				ny = node.y + dy[i];
 
-				if(nx<0 || ny<0 || nx>=l || ny>=w) continue;
-
-				if(map[nx][ny].equals("L") && !visited[nx][ny]) {
-					visited[nx][ny] = true;
-					queue.add(new Node(nx, ny, node.d+1));
+				if(nx < 0 || ny < 0 || nx >= l || ny >= w || visited[nx][ny] || maps[nx][ny].equals("W")) {
+					continue;
 				}
+
+				visited[nx][ny] = true;
+				queue.add(new Node(nx, ny, node.v+1));
 			}
 		}
-		return answer;
 	}
-
 }
-
