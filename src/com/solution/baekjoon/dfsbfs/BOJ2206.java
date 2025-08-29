adb70ec0e1331e51f5b13cf4fd11f54cae632b8b
dnd2dnd <tkfdkskarl56@naver.com>
2025-08-29 17:35:27 +0900

    [골드 3] 벽 부수고 이동하기

--- src/com/solution/baekjoon/dfsbfs/BOJ2206.java
+++ src/com/solution/baekjoon/dfsbfs/BOJ2206.java
@@ -7,79 +7,83 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-/**
- * 백준 2206 벽 부수고 이동하기, 골드 3
- */
 public class BOJ2206 {
-	static int n, m;
-	static int[][] map;
-	static int[] sx = {1, -1, 0, 0};
-	static int[] sy = {0, 0, 1, -1};
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static int N, M;
+    public static int[] dx = {1, -1, 0 ,0};
+    public static int[] dy = {0, 0, 1, -1};
+    public static int[][] maps;
+    public static int min = Integer.MAX_VALUE;
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			String a[] = st.nextToken().split("");
-			for(int j=0; j<m; j++) {
-				map[i][j] = Integer.parseInt(a[j]);
-			}
-		}
+    public static class Node {
+        int x;
+        int y;
+        int cnt;
+        boolean b;
 
-		int ans = Integer.MAX_VALUE;
-		boolean[][][] visited = new boolean[n][m][2];
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(0,0,1, false));
+        public Node(int x, int y, int cnt, boolean b) {
+            this.x = x;
+            this.y = y;
+            this.cnt = cnt;
+            this.b = b;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		while(!q.isEmpty()) {
-			Node node = q.poll();
+        N  = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-			if(node.x == n-1 && node.y == m-1) {
-				System.out.println(node.d);
-				return;
-			}
+        maps = new int[N][M];
+        for (int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String a[] = st.nextToken().split("");
+            for(int j=0; j<M; j++) {
+                maps[i][j] = Integer.parseInt(a[j]);
+            }
+        }
 
-			int dx, dy;
-			for(int i=0; i<4; i++) {
-				dx = node.x + sx[i];
-				dy = node.y + sy[i];
+        bfs(0, 0);
+    }
 
-				if(dx<0 || dy<0 || dx>=n || dy>=m) continue;
+    public static void bfs(int x, int y) {
+        boolean[][][] visited = new boolean[N][M][2];
+        visited[x][y][0] = true;
 
-				if(map[dx][dy]==0) {
-					if(!visited[dx][dy][0] && !node.c) {
-						q.add(new Node(dx, dy, node.d+1, false));
-						visited[dx][dy][0]=true;
-					} else if(!visited[dx][dy][1] && node.c) {
-						q.add(new Node(dx, dy, node.d+1, true));
-						visited[dx][dy][1]=true;
-					}
-				} else if (map[dx][dy]==1) {
-					if(!node.c) {
-						q.add(new Node(dx, dy, node.d+1, true));
-						visited[dx][dy][1]=true;
-					}
-				}
-			}
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(x, y, 1, false));
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            int nx, ny;
+            if (node.x == N-1 && node.y == M-1) {
+                System.out.println(node.cnt);
+                break;
+            }
+            for (int i=0; i<4; i++) {
+                nx = node.x + dx[i];
+                ny = node.y + dy[i];
 
-		}
-		System.out.println(-1);
-	}
+                if (nx<0 || ny<0 || nx>=N || ny >=M) {
+                    continue;
+                }
 
+                if (maps[nx][ny] == 1) {
+                    if (!node.b) {
+                        visited[nx][ny][1] = true;
+                        queue.add(new Node(nx, ny, node.cnt+1, true));
+                    }
+                } else if (maps[nx][ny] == 0) {
+                    if (!visited[nx][ny][0] && !node.b) {
+                        visited[nx][ny][0] = true;
+                        queue.add(new Node(nx, ny, node.cnt+1, node.b));
+                    } else if (!visited[nx][ny][1] && node.b) {
+                        visited[nx][ny][1] = true;
+                        queue.add(new Node(nx, ny, node.cnt+1, node.b));
+                    }
+                }
 
-	static class Node {
-		int x, y, d;
-		boolean c;
+            }
+        }
 
-		public Node(int x, int y, int d, boolean c) {
-			this.x = x;
-			this.y = y;
-			this.d = d;
-			this.c = c;
-		}
-	}
+    }
 }
