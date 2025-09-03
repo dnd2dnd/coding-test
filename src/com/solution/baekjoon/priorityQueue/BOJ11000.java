c6d5095c4d580351af8c81ab0bcc181c8e8a567a
dnd2dnd <tkfdkskarl56@naver.com>
2025-09-03 17:30:52 +0900

    [골드 4] 강의실 배정

--- src/com/solution/baekjoon/priorityQueue/BOJ11000.java
+++ src/com/solution/baekjoon/priorityQueue/BOJ11000.java
@@ -3,57 +3,32 @@ package com.solution.baekjoon.priorityQueue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.annotation.Documented;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-/**
- * 백준 11000, 강의실 배정, 골드 5
- */
 public class BOJ11000 {
-	public static class Node implements Comparable<Node>{
-		int st, et;
+    public static class Node {
+        int x, y;
 
-		public Node(int st, int et) {
-			this.st = st;
-			this.et = et;
-		}
+        public Node (int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
 
-		@Override
-		public int compareTo(Node o) {
-			if(this.st == o.st) {
-				return this.et - o.et;
-			}
-			return this.st - o.st;
-		}
-	}
+        Node[] node = new Node[n];
+        for (int i =0; i<n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-	static int N;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		N = Integer.parseInt(br.readLine());
-		Node[] nodes = new Node[N];
-		int a, b;
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			a = Integer.parseInt(st.nextToken());
-			b = Integer.parseInt(st.nextToken());
+            node[i] = new Node(x, y);
+        }
+        Arrays.sort(node);
 
-			nodes[i] = new Node(a, b);
-		}
-
-		Arrays.sort(nodes);
-		PriorityQueue<Integer> pq = new PriorityQueue<>();
-		pq.add(nodes[0].et);
-
-		for(int i=1; i<N; i++) {
-			if(nodes[i].st >= pq.peek()) {
-				pq.poll();
-			}
-			pq.add(nodes[i].et);
-		}
-		System.out.println(pq.size());
-	}
+    }
 }
