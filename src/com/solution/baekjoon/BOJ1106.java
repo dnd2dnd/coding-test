cb6d1cbec17b7e2ed389af851029e46183d20100
dnd2dnd <tkfdkskarl56@naver.com>
2025-12-03 22:51:33 +0900

    [골드 4] 호텔  - 아이디어 부족

--- src/com/solution/baekjoon/BOJ1106.java
+++ src/com/solution/baekjoon/BOJ1106.java
@@ -7,44 +7,42 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class BOJ1106 {
-    public static class Node {
-        int person, money;
-
-        public Node (int person, int money) {
-            this.person = person;
-            this.money = money;
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int C = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
-        Node[] nodes = new Node[N+1];
-        for (int i = 1; i <= N; i++) {
+        Node[] nodes = new Node[n];
+        for (int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
-
-            int m = Integer.parseInt(st.nextToken());
-            int p = Integer.parseInt(st.nextToken());
-            nodes[i] = new Node(p, m);
-
+            int y = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            nodes[i] = new Node(x, y);
         }
-        final int INF = 1_000_000_000;
-        int[] dp = new int[C + 1];
-        Arrays.fill(dp, INF);
-        dp[0] = 0;
-        for (int i=1; i<=C; i++) {
-            for (int j=1; j<=N; j++) {
-                if (i-nodes[j].person >= 0) {
-                    dp[i] = Math.min(dp[i-nodes[j].person] + nodes[j].money, dp[i]);
-                } else {
-                    dp[i] = Math.min(dp[0] + nodes[j].money, dp[i]);
-                }
 
+        int[] dp = new int[c+1];
+        Arrays.fill(dp, 1_000_000_000);
+        dp[0] = 0;
+        for (int i=1; i<=c; i++) {
+            for (int j=0; j<n; j++) {
+                if (i - nodes[j].x >= 0) {
+                    dp[i] = Math.min(dp[i-nodes[j].x] + nodes[j].y, dp[i]);
+                } else {
+                    dp[i] = Math.min(dp[0] + nodes[j].y, dp[i]);
+                }
             }
         }
-        System.out.println(dp[C]);
+        System.out.println(dp[c]);
+    }
+
+    public static class Node {
+        int x, y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
     }
 }
