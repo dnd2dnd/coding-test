5fd5e25834a76806427a5999caf24d82ea25024a
dnd2dnd <tkfdkskarl56@naver.com>
2025-12-06 22:54:19 +0900

    [브론즈 1] 나무 조각  - 성공

---
+++ src/com/solution/baekjoon/BOJ.java
@@ -0,0 +1,32 @@
+package com.solution.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] strs = br.readLine().split(" ");
+
+        int[] nums = new int[5];
+        for (int i=0; i<5; i++) {
+            nums[i] = Integer.parseInt(strs[i]);
+        }
+
+        for (int i=0; i<5; i++) {
+            for (int j=0; j<4; j++) {
+                if (nums[j] > nums[j+1]) {
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = temp;
+
+                    for (int k=0; k<5; k++) {
+                        System.out.print(nums[k] + " ");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+}
