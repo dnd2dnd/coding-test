877b86aeadb1b7a9a9bec2de9b38bccda5d741dd
dnd2dnd <tkfdkskarl56@naver.com>
2025-12-09 07:15:27 +0900

    [실버 1] RGB 거리  - 재풀이 (아이디어 부족)

--- src/com/solution/baekjoon/dp/BOJ1149.java
+++ src/com/solution/baekjoon/dp/BOJ1149.java
@@ -6,49 +6,31 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ1149 {
-	static int n;
-	static int[][] home;
-	static int min = Integer.MAX_VALUE;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		home = new int[n][4];
-		StringTokenizer st;
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=1; j<=3; j++) {
-				home[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int[][] dp = new int[n][4];
-		dp[0][1] = home[0][1];
-		dp[0][2] = home[0][2];
-		dp[0][3] = home[0][3];
-		for(int i=1; i<n; i++) {
-			dp[i][1] = Math.min(dp[i-1][2] + home[i][1], dp[i-1][3] + home[i][1]);
-			dp[i][2] = Math.min(dp[i-1][1] + home[i][2], dp[i-1][3] + home[i][2]);
-			dp[i][3] = Math.min(dp[i-1][1] + home[i][3], dp[i-1][2] + home[i][3]);
-		}
-		System.out.println(Math.min(Math.min(dp[n-1][1], dp[n-1][2]), dp[n-1][3]));
-		// dfs(0, 0, 0);
-		// System.out.println(min);
-	}
-	// public static void dfs(int d, int x, int cost) {
-	// 	if(x==0) {
-	// 		for(int i=1; i<=3; i++) {
-	// 			dfs(d+1, i, cost+home[d][i]);
-	// 		}
-	// 	}
-	// 	if(d==n) {
-	// 		min = Math.min(min, cost);
-	// 		return;
-	// 	}
-	//
-	// 	for(int i=1; i<=3; i++) {
-	// 		if(x!=i) {
-	// 			dfs(d+1, i, cost+home[d][i]);
-	// 		}
-	// 	}
-	// }
+        int n = Integer.parseInt(st.nextToken());
+
+        int[][] colors = new int[n][3];
+        int[][] dp = new int[n][3];
+        for (int i=0; i<n; i++) {
+            String[] strs = br.readLine().split(" ");
+            for(int j=0; j<3; j++) {
+                colors[i][j] = Integer.parseInt(strs[j]);
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        dp[0][0] = colors[0][0];
+        dp[0][1] = colors[0][1];
+        dp[0][2] = colors[0][2];
+        for (int i=1; i<n; i++) {
+            dp[i][0] = colors[i][0] + Math.min(dp[i-1][1], dp[i-1][2]);
+            dp[i][1] = colors[i][1] + Math.min(dp[i-1][0], dp[i-1][2]);
+            dp[i][2] = colors[i][2] + Math.min(dp[i-1][0], dp[i-1][1]);
+        }
+
+        System.out.println(Math.min(dp[n-1][2], Math.min(dp[n-1][0], dp[n-1][1])));
+    }
 }
