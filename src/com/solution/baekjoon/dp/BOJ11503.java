bdcb250863384a99135af1c57f31efafab9e8c30
dnd2dnd <tkfdkskarl56@naver.com>
2025-12-09 21:32:51 +0900

    [실버 2] 가장 긴 증가하는 부분 수열  - 풀지못했음

---
+++ src/com/solution/baekjoon/dp/BOJ11503.java
@@ -0,0 +1,34 @@
+package com.solution.baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ11503 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] nums = new int[n];
+        String[] strs = br.readLine().split(" ");
+        for (int i=0; i<n; i++) {
+            nums[i] = Integer.parseInt(strs[i]);
+        }
+
+        int[] dp = new int[n];
+        int max = Integer.MIN_VALUE;
+        for (int i=0; i<n; i++) {
+            dp[i] = 1;
+            for (int j=0; j<i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j-1] +1);
+                }
+            }
+            max = Math.max(dp[i], max);
+        }
+        System.out.println(max);
+    }
+}
