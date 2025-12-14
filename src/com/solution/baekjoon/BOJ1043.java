27735c54367e847f16f24defed3846671d7a60c1
dnd2dnd <tkfdkskarl56@naver.com>
2025-12-14 21:30:58 +0900

    [골드 4] 거짓말  - 못풀음

--- src/com/solution/baekjoon/BOJ1043.java
+++ src/com/solution/baekjoon/BOJ1043.java
@@ -6,82 +6,81 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-/**
- * 백준 1043, 거짓말, 골드 4
- */
 public class BOJ1043 {
-	static int[] parent;
-	static boolean[] check;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
+    public static int[] parent;
+    public static boolean[] truth;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        parent = new int[n+1];
+        truth = new boolean[n+1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
 
-		parent = new int[n+1];
-		for(int i=0; i<=n; i++) {
-			parent[i] = i;
-		}
+        st = new StringTokenizer(br.readLine());
+        int truthCount = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < truthCount; i++) {
+            int person = Integer.parseInt(st.nextToken());
+            truth[person] = true;
+        }
 
-		check = new boolean[n+1];
-		st = new StringTokenizer(br.readLine());
-		int A = Integer.parseInt(st.nextToken());
-		if(A>0) {
-			for(int i=0; i<A; i++) {
-				check[Integer.parseInt(st.nextToken())] = true;
-			}
-		}
+        ArrayList<ArrayList<Integer>> partyList = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int partySize = Integer.parseInt(st.nextToken());
 
-		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-		for(int i=0; i<m; i++) {
-			list.add(new ArrayList<>());
-		}
+            ArrayList<Integer> party = new ArrayList<>();
+            int firstPerson = Integer.parseInt(st.nextToken());
+            party.add(firstPerson);
 
-		for(int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			ArrayList<Integer> temp = new ArrayList<>();
-			for(int j=0; j<a; j++) {
-				int k = Integer.parseInt(st.nextToken());
-				list.get(i).add(k);
-				temp.add(k);
-			}
+            for (int j = 1; j < partySize; j++) {
+                int nextPerson = Integer.parseInt(st.nextToken());
+                union(firstPerson, nextPerson);
+                party.add(nextPerson);
+            }
+            partyList.add(party);
+        }
 
-			for(int j=0; j < temp.size()-1; j++) {
-				union(temp.get(j), temp.get(j+1));
-			}
-		}
+        int cnt = 0;
+        for (ArrayList<Integer> party : partyList) {
+            boolean safe = true;
 
-		for(int i=1; i<=n; i++) {
-			if(check[i]) {
-				check[find(i)] = true;
-			}
-		}
+            for (int p : party) {
+                if(truth[find(p)]) {
+                    safe = false;
+                    break;
+                }
+            }
 
-		int cnt = 0;
-		for(int i=0; i<m; i++) {
-			boolean b = false;
-			for (Integer j : list.get(i)) {
-				if(check[find(j)]) {
-					b = true;
-					break;
-				}
-			}
-			if(!b) cnt++;
-		}
+            if (safe) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
 
-		System.out.println(cnt);
-	}
-	public static void union(int x, int y) {
-		x = find(x);
-		y = find(y);
+    }
 
-		if(x==y) return;
-		parent[y] = x;
+    public static int find(int x) {
+        if (parent[x] == x) {
+            return x;
+        }
 
-	}
-	public static int find(int x) {
-		if(parent[x] == x) return x;
-		return parent[x] = find(parent[x]);
-	}
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+
+        if (rootA != rootB) {
+            parent[rootA] = rootB;
+        }
+        if (truth[rootA] || truth[rootB]) {
+            truth[rootB] = true;
+        }
+    }
+
 }
