22c64b2151a65e599b704b9c12fb9b612354ade1
dnd2dnd <tkfdkskarl56@naver.com>
2026-01-05 20:45:50 +0900

    [골드 4] 문자열 폭발  - 재풀이 실패

--- src/com/solution/baekjoon/string/BOJ9935.java
+++ src/com/solution/baekjoon/string/BOJ9935.java
@@ -5,45 +5,42 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-/**
- *  백준 9935, 문자열 폭발, 골드 4
- */
 public class BOJ9935 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-		String b = br.readLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String bomb = br.readLine();
 
-		Stack<Character> stack = new Stack<>();
-		for(int i=0; i<s.length(); i++) {
-			stack.push(s.charAt(i));
-			if(stack.size()>=b.length()) {
-				int k=0;
-				boolean check = true;
-				for(int j=b.length()-1; j>=0; j--) {
-					k++;
-					if(b.charAt(j) != stack.get(stack.size()-k)) {
-						check = false;
-						break;
-					}
-				}
-				if(check) {
-					for(int j=0; j<b.length(); j++) {
-						stack.pop();
-					}
-				}
-			}
-		}
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            stack.push(s.charAt(i));
 
-		StringBuilder stringBuilder = new StringBuilder();
-		for (char ch : stack){
-			stringBuilder.append(ch);
-		}
+            if (stack.size() >= bomb.length()) {
+                int k = 0;
+                boolean check = true;
+                for (int j=bomb.length()-1; j>=0; j--) {
+                    k++;
+                    if (bomb.charAt(j) != stack.get(stack.size()-k)) {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check) {
+                    for (int j = 0; j < bomb.length(); j++) {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Character c : stack) {
+            sb.append(c);
+        }
 
-		if(stringBuilder.length()==0) {
-			System.out.println("FRULA");
-		} else {
-			System.out.println(stringBuilder);
-		}
-	}
+        if(sb.isEmpty()) {
+            System.out.println("FRULA");
+        } else {
+            System.out.println(sb);
+        }
+    }
 }
