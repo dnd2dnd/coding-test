5b450f3b5484b3c9db55bbb7404367c101e84249
dnd2dnd <tkfdkskarl56@naver.com>
2026-01-18 21:28:18 +0900

    [실버 1] 스티커  - 완료

--- src/com/solution/baekjoon/dp/BOJ9465.java
+++ src/com/solution/baekjoon/dp/BOJ9465.java
@@ -5,37 +5,35 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-/**
- * 백준 9465, 스티커, 실버 1
- */
 public class BOJ9465 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
 
-		for(int t=0; t<T; t++) {
-			int n = Integer.parseInt(br.readLine());
-			int[][] nums = new int[2][n+2];
-			StringTokenizer st;
-			for(int i=0; i<2; i++) {
-				st = new StringTokenizer(br.readLine());
-				for(int j=2; j<n+2; j++) {
-					nums[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
+        for(int i=0; i<t; i++) {
+            int n = Integer.parseInt(br.readLine());
+            int[][] maps = new int[2][n];
+            int[][] dp = new int[2][n];
+            for (int j=0; j<2; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int k=0; k<n; k++) {
+                    maps[j][k] = Integer.parseInt(st.nextToken());
+                }
+            }
+            dp[0][0] = maps[0][0];
+            dp[1][0] = maps[1][0];
 
-			int[][] dp = new int[2][n+2];
-			for(int j=2; j<n+2; j++) {
-				for(int i=0; i<2; i++) {
-					if(i==0) {
-						dp[i][j] = Math.max(dp[i][j], Math.max(dp[1][j-1], dp[1][j-2])) + nums[i][j];
-					} else {
-						dp[i][j] = Math.max(dp[i][j], Math.max(dp[0][j-1], dp[0][j-2])) + nums[i][j];
-					}
-				}
-			}
+            if (n>1) {
+                dp[0][1] = dp[1][0] + maps[0][1];
+                dp[1][1] = maps[0][0] + maps[1][1];
 
-			System.out.println(Math.max(dp[0][n+1], dp[1][n+1]));
-		}
-	}
+                for (int q=2; q<n; q++) {
+                    dp[0][q] = Math.max(dp[1][q-1], dp[1][q-2]) + maps[0][q];
+                    dp[1][q] = Math.max(dp[0][q-1], dp[0][q-2]) + maps[1][q];
+                }
+            }
+
+            System.out.println(Math.max(dp[0][n-1], dp[1][n-1]));
+        }
+    }
 }
