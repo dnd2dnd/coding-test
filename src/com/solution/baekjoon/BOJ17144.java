5ff77d2a176f631f1ebe84b2a9765d28ce8f4876
dnd2dnd <tkfdkskarl56@naver.com>
2026-01-23 23:06:19 +0900

    [골드 4] BOJ17144 미세먼지 안녕!  - 완료

--- src/com/solution/baekjoon/BOJ17144.java
+++ src/com/solution/baekjoon/BOJ17144.java
@@ -3,107 +3,135 @@ package com.solution.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
-/**
- *  백준 17144, 미세먼지 안녕!, 골드 4
- */
 public class BOJ17144 {
-	static int r, c, t;
-	static int cleaner;
-	static int[] dx = {1, -1, 0, 0};
-	static int[] dy = {0, 0, 1, -1};
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static int[][] maps;
+    public static List<Node> nodes = new ArrayList<>();
+    public static int r, c, t;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
 
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		t = Integer.parseInt(st.nextToken());
+        maps = new int[r][c];
+        for (int i=0; i<r; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=0; j<c; j++) {
+                maps[i][j] = Integer.parseInt(st.nextToken());
+                if (maps[i][j] == -1) {
+                    nodes.add(new Node(i, j));
+                }
+            }
+        }
 
-		cleaner = -1;
-		int[][] maps = new int[r][c];
-		for(int i=0; i<r; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<c; j++) {
-				maps[i][j] = Integer.parseInt(st.nextToken());
-				if(maps[i][j]==-1 && cleaner == -1) {
-					cleaner = i;
-				}
-			}
-		}
-		rec(0, maps);
-	}
-	public static void rec(int s, int[][] maps) {
-		if(s==t) {
-			int sum = 0;
-			for(int i=0; i<r; i++) {
-				for(int j=0; j<c; j++) {
-					if(maps[i][j]>=1) {
-						sum += maps[i][j];
-					}
-				}
-			}
-			System.out.println(sum);
-			return;
-		}
-		int[][] temp = new int[r][c];
-		for(int i=0; i<r; i++) {
-			for(int j=0; j<c; j++) {
-				int v = maps[i][j];
-				if(v==-1) {
-					temp[i][j] = v;
-					continue;
-				}
-				int q = v/5;
-				int cnt = 0;
-				int nx, ny;
-				for(int k=0; k<4; k++) {
-					nx = i + dx[k];
-					ny = j + dy[k];
+        for (int i=0; i<t; i++) {
+            spread();
+            circle();
+        }
 
-					if(nx<0 || ny<0 || nx>=r || ny>=c || maps[nx][ny]==-1) continue;
+        int cnt = 0;
+        for (int i=0; i<r; i++) {
+            for (int j=0; j<c; j++) {
+                cnt += maps[i][j];
+            }
+        }
+        System.out.println(cnt+2);
 
-					cnt++;
-					temp[nx][ny] += q;
-				}
-				temp[i][j] += (v - q*cnt);
-			}
-		}
+    }
+    public static void spread() {
+        int[][] newMaps = new int[r][c];
+        for (int i=0; i<r; i++) {
+            for (int j=0; j<c; j++) {
+                if (maps[i][j] == -1) {
+                    newMaps[i][j] = maps[i][j];
+                    continue;
+                }
 
-		rec(s+1, airClear(temp));
-	}
-	public static int[][] airClear(int[][] temp) {
-		int x = cleaner;
+                int check = 0;
+                int value = maps[i][j]/5;
+                if (i-1 >= 0 && maps[i-1][j] != -1) {
+                    check++;
+                    newMaps[i-1][j] += value;
+                }
+                if (j+1 < c && maps[i][j+1] != -1) {
+                    check++;
+                    newMaps[i][j+1] += value;
+                }
+                if (j-1 >= 0 && maps[i][j-1] != -1) {
+                    check++;
+                    newMaps[i][j-1] += value;
+                }
+                if (i+1 < r && maps[i+1][j] != -1) {
+                    check++;
+                    newMaps[i+1][j] += value;
+                }
 
-		for(int i=x-1; i>0; i--) {
-			temp[i][0] = temp[i-1][0];
-		}
-		for(int i=0; i<c-1; i++) {
-			temp[0][i] = temp[0][i+1];
-		}
-		for(int i=0; i<x; i++) {
-			temp[i][c-1] = temp[i+1][c-1];
-		}
-		for(int i=c-1; i>1; i--) {
-			temp[x][i] = temp[x][i-1];
-		}
-		temp[x][1] = 0;
+                newMaps[i][j] += maps[i][j] - (value * check);
+            }
+        }
 
-		for(int i=x+2; i<r-1; i++) {
-			temp[i][0] = temp[i+1][0];
-		}
-		for(int i=0; i<c-1; i++) {
-			temp[r-1][i] = temp[r-1][i+1];
-		}
-		for(int i=r-1; i>x+1; i--) {
-			temp[i][c-1] = temp[i-1][c-1];
-		}
-		for(int i=c-1; i>1; i--)
-			temp[x+1][i] = temp[x+1][i-1];
-		temp[x+1][1] = 0;
+        maps = new int[r][c];
+        for (int i=0; i<r; i++) {
+            for (int j=0; j<c; j++) {
+                maps[i][j] = newMaps[i][j];
+            }
+        }
 
+    }
 
-		return temp;
-	}
+    public static void circle() {
+        Node fNode = nodes.getFirst();
+        int fx = fNode.x;
+
+        for (int i=fx-1; i>0; i--) {
+            maps[i][0] = maps[i-1][0];
+        }
+        for (int i=0; i<c-1; i++) {
+            maps[0][i] = maps[0][i+1];
+        }
+        for (int i=0; i<fx; i++) {
+            maps[i][c-1] = maps[i+1][c-1];
+        }
+        for (int i=c-1; i>0; i--) {
+            if (i==1) {
+                maps[fx][1] = 0;
+            } else {
+                maps[fx][i] = maps[fx][i-1];
+            }
+        }
+
+        fNode = nodes.get(1);
+        fx = fNode.x;
+
+        for (int i=fx+1; i<r-1; i++) {
+            maps[i][0] = maps[i+1][0];
+        }
+        for (int i=0; i<c-1; i++) {
+            maps[r-1][i] = maps[r-1][i+1];
+        }
+        for (int i=r-1; i>fx; i--) {
+            maps[i][c-1] = maps[i-1][c-1];
+        }
+        for (int i=c-1; i>0; i--) {
+            if (i==1) {
+                maps[fx][1] = 0;
+            } else {
+                maps[fx][i] = maps[fx][i-1];
+            }
+        }
+    }
+
+    public static class Node {
+        int x, y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
