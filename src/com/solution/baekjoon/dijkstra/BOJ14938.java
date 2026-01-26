f9183fa77373379b587040aa514f3580d1d03b58
dnd2dnd <tkfdkskarl56@naver.com>
2026-01-26 19:40:16 +0900

    [골드 4] BOJ14938 서강그라운드  - 못풀음

--- src/com/solution/baekjoon/dijkstra/BOJ14938.java
+++ src/com/solution/baekjoon/dijkstra/BOJ14938.java
@@ -4,87 +4,87 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-/**
- *  백준 14938, 서강그라운드, 골드 5
- */
 public class BOJ14938 {
-	public static class Node {
-		int y, c;
+    public static int n, m, r, sum = 0;
+    public static int[] place;
+    public static List<List<Node>> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		public Node(int y, int c) {
-			this.y = y;
-			this.c = c;
-		}
-	}
-	static int n, m, r, max = Integer.MIN_VALUE;
-	static int[] area;
-	static ArrayList<ArrayList<Node>> list;
-	static boolean[] visited;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		r = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
 
-		list = new ArrayList<>();
-		area = new int[n+1];
-		st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=n; i++) {
-			area[i] = Integer.parseInt(st.nextToken());
-		}
+        place = new int[n+1];
+        st = new StringTokenizer(br.readLine());
 
-		for(int i=0; i<=n; i++) {
-			list.add(new ArrayList<>());
-		}
+        list.add(new ArrayList<>());
+        for (int i=0; i<n; i++) {
+            place[i+1] = Integer.parseInt(st.nextToken());
+            list.add(new ArrayList<>());
+        }
 
-		for(int i=0; i<r; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
+        for (int i=0; i<r; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-			list.get(a).add(new Node(b, c));
-			list.get(b).add(new Node(a, c));
-		}
+            list.get(a).add(new Node(b, c));
+            list.get(b).add(new Node(a, c));
+        }
 
-		for(int i=1; i<=n; i++) {
-			visited = new boolean[n+1];
-			max = Math.max(max, rec(i));
-		}
+        for(int i=1; i<=n; i++) {
+            bfs(i);
+        }
+        System.out.println(sum);
+    }
 
-		System.out.println(max);
+    public static void bfs(int x) {
+        PriorityQueue<Node> queue = new PriorityQueue<>((a, b) -> a.w-b.w);
+        queue.add(new Node(x, 0));
+        int[] dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[x] = 0;
 
-	}
-	public static int rec(int x) {
-		int cnt = 0;
-		int[] recArea = new int[n+1];
-		for(int i=1; i<=n; i++) {
-			recArea[i] = m+1;
-		}
-		PriorityQueue<Node> queue = new PriorityQueue<>(Comparator.comparingInt(o -> o.c));
-		queue.add(new Node(x, 0));
-		recArea[x] = 0;
+        while(!queue.isEmpty()) {
+            Node cur = queue.poll();
+            if (cur.w > dist[cur.end]) {
+                continue;
+            }
 
-		while (!queue.isEmpty()) {
-			Node node = queue.poll();
+            for (Node node : list.get(cur.end)) {
+                int cost = node.w + cur.w;
 
-			if(visited[node.y]) continue;
+                if (cost < dist[node.end]) {
+                    dist[node.end] = cost;
+                    queue.add(new Node(node.end, cost));
+                }
+            }
+        }
 
-			visited[node.y] = true;
-			cnt += area[node.y];
-			for (Node next : list.get(node.y)) {
-				if(recArea[node.y] + next.c < recArea[next.y] && !visited[next.y]) {
-					recArea[next.y] = recArea[node.y] + next.c;
-					queue.add(new Node(next.y, recArea[next.y]));
-				}
-			}
-		}
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] <= m) {
+                cnt += place[i];
+            }
+        }
 
-		return cnt;
-	}
+        sum = Math.max(cnt, sum);
+    }
+
+    public static class Node {
+        int end, w;
+
+        public Node(int end, int w) {
+            this.end = end;
+            this.w = w;
+        }
+    }
 }
