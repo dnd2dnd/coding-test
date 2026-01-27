edd3e57a5cb50bacac8cffa3e181bd36698012a8
dnd2dnd <tkfdkskarl56@naver.com>
2026-01-27 18:57:44 +0900

    [골드 3] BOJ2638 치즈  - 못풀음

--- src/com/solution/baekjoon/dfsbfs/BOJ2638.java
+++ src/com/solution/baekjoon/dfsbfs/BOJ2638.java
@@ -7,80 +7,72 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-/**
- * 백준 2638번 치즈, 골드 3
- */
 public class BOJ2638 {
-	static int n, m;
-	static int[][] place;
-	static int cheese;
-	static int[] dx = {1, -1, 0, 0};
-	static int[] dy = {0, 0, 1, -1};
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static int[] dx = {1, -1, 0, 0};
+    public static int[] dy = {0, 0, 1, -1};
+    public static int[][] maps;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        maps = new int[n][m];
+        boolean[][][] visited = new boolean[n][m][2];
 
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		place = new int[n][m];
-		cheese = 0;
-		for(int i=0; i<n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<m; j++) {
-				place[i][j] = Integer.parseInt(st.nextToken());
-				if(place[i][j] == 1) {
-					cheese++;
-				}
-			}
-		}
+        for (int i=0; i<n; i++) {
+            String[] strs = br.readLine().split("");
+            for (int j=0; j<m; j++) {
+                maps[i][j] = Integer.parseInt(strs[j]);
+            }
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0, 0, 1, false));
+        visited[0][0][0] = true;
+        int cnt = -1;
+        while(!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (node.x == n-1 && node.y == m-1) {
+                cnt = node.z;
+                break;
+            }
 
-		int lastCheese = 0;
-		int cnt =0;
-		while(cheese!=0) {
-			lastCheese = cheese;
-			bfs();
-			cnt++;
-		}
-		System.out.println(cnt);
-	}
+            int nx, ny;
+            for (int i=0; i<4; i++) {
+                nx = node.x + dx[i];
+                ny = node.y + dy[i];
 
-	public static void bfs() {
-		Queue<Node> queue = new LinkedList<>();
-		queue.add(new Node(0, 0));
-		int[][] check = new int[n][m];
-		while(!queue.isEmpty()) {
-			Node node = queue.poll();
+                if (nx<0 || ny<0 || nx>=n || ny>=m) {
+                    continue;
+                }
 
-			int nx, ny;
-			for(int i=0; i<4; i++) {
-				nx = node.x + dx[i];
-				ny = node.y + dy[i];
-				
-				if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
+                if (maps[nx][ny] == 1) {
+                    if (!node.broken) {
+                        visited[nx][ny][1] = true;
+                        queue.add(new Node(nx, ny, node.z+1, true));
+                    }
+                } else if (maps[nx][ny] == 0){
+                    if (!visited[nx][ny][0] && !node.broken) {
+                        visited[nx][ny][0] = true;
+                        queue.add(new Node(nx, ny, node.z+1, node.broken));
+                    } else if (!visited[nx][ny][1] && node.broken) {
+                        visited[nx][ny][1] = true;
+                        queue.add(new Node(nx, ny, node.z+1, node.broken));
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
 
-				if(check[nx][ny]!=2) {
-					if(place[nx][ny]==0) {
-						queue.add(new Node(nx, ny));
-						check[nx][ny]=2;
-					} else if (place[nx][ny]==1) {
-						check[nx][ny]+=1;
-						if(check[nx][ny]==2) {
-							place[nx][ny]=0;
-							cheese--;
-						}
-					}
-				}
-			}
-		}
-	}
+    public static class Node {
+        int x, y, z;
+        boolean broken;
 
-
-	public static class Node {
-		int x, y;
-
-		public Node(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+        public Node(int x, int y, int z, boolean broken) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.broken = broken;
+        }
+    }
 }
