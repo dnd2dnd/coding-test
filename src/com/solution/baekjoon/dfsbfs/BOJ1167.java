0e7833f62c12a532c904dc075b62ee03465009f2
dnd2dnd <tkfdkskarl56@naver.com>
2026-01-28 20:56:33 +0900

    [골드 2] BOJ1167 트리의 지름  - 힌트 보고 풀음

--- src/com/solution/baekjoon/dfsbfs/BOJ1167.java
+++ src/com/solution/baekjoon/dfsbfs/BOJ1167.java
@@ -4,64 +4,70 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-/**
- * 백준 1167, 트리의 지름, 골드 2
- */
 public class BOJ1167 {
-	public static class Node {
-		int next, value;
+    public static int v, index, cnt = Integer.MIN_VALUE;
+    public static List<List<Node>> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        v = Integer.parseInt(st.nextToken());
 
-		public Node(int next, int value) {
-			this.next = next;
-			this.value = value;
-		}
-	}
-	public static int max = Integer.MIN_VALUE;
-	public static int maxNode;
-	public static boolean[] visited;
-	public static ArrayList<ArrayList<Node>> list = new ArrayList<>();
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int v = Integer.parseInt(br.readLine());
+        list.add(new ArrayList<>());
+        for (int i=0; i<v; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int i=1; i<=v; i++) {
+            st = new StringTokenizer(br.readLine());
+            int idx = Integer.parseInt(st.nextToken());
+            while (true) {
+                int x = Integer.parseInt(st.nextToken());
+                if (x==-1) {
+                    break;
+                }
+                int y = Integer.parseInt(st.nextToken());
 
-		for(int i=0; i<=v; i++) {
-			list.add(new ArrayList<>());
-		}
+                list.get(idx).add(new Node(x, y));
+            }
+        }
 
-		StringTokenizer st;
-		for(int i=1; i<=v; i++) {
-			st = new StringTokenizer(br.readLine());
-			int idx = Integer.parseInt(st.nextToken());
-			while (true) {
-				int x = Integer.parseInt(st.nextToken());
-				if (x==-1) {
-					break;
-				}
-				int y = Integer.parseInt(st.nextToken());
+        bfs(1);
+        bfs(index);
+        System.out.println(cnt);
+    }
 
-				list.get(idx).add(new Node(x, y));
-			}
-		}
+    public static void bfs(int x) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(x, 0));
+        boolean[] visited = new boolean[v+1];
+        visited[x] = true;
+        while(!queue.isEmpty()) {
+            Node cur = queue.poll();
+            if (cnt < cur.w) {
+                cnt = cur.w;
+                index = cur.end;
+            }
 
-		visited = new boolean[v+1];
-		dfs(1, 0);
-		visited = new boolean[v+1];
-		dfs(maxNode, 0);
-		System.out.println(max);
-	}
-	public static void dfs(int x, int sum) {
-		visited[x] = true;
-		for (Node node : list.get(x)) {
-			if(!visited[node.next]) {
-				dfs(node.next, sum + node.value);
-			}
-		}
-		if(max < sum) {
-			max = sum;
-			maxNode = x;
-		}
-	}
+            for (Node node : list.get(cur.end)) {
+                if (!visited[node.end]) {
+                    visited[node.end] = true;
+                    queue.add(new Node(node.end, cur.w + node.w));
+                }
+            }
+        }
+    }
+
+    public static class Node {
+        int end, w;
+
+        public Node(int end, int w) {
+            this.end = end;
+            this.w = w;
+        }
+    }
+
 }
-
