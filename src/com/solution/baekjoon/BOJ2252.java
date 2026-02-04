9c92df71314cbb0918ec6972353aaaf41470551c
dnd2dnd <tkfdkskarl56@naver.com>
2026-02-04 22:11:31 +0900

    [골드 3] 줄 세우기  - 풀음

--- src/com/solution/baekjoon/BOJ2252.java
+++ src/com/solution/baekjoon/BOJ2252.java
@@ -3,50 +3,55 @@ package com.solution.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class BOJ2252 {
-    static StringBuffer sb = new StringBuffer();
-    static int[] edge;
-    static boolean[] visited;
-    static List<List<Integer>> list = new ArrayList<>();
+    public static List<List<Integer>> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-
-        edge = new int[n+1];
-        visited = new boolean[n+1];
-        for(int i=0; i<=n; i++) {
+        int[] nodes = new int[n+1];
+        for (int i=0; i<=n; i++) {
             list.add(new ArrayList<>());
         }
 
-        for(int i=0; i<m; i++) {
+        for (int i=0; i<m; i++) {
             st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             list.get(a).add(b);
-            edge[b]++;
+            nodes[b]++;
         }
 
-        for(int i=1; i<=n; i++) {
-            if(edge[i] == 0 && !visited[i]) {
-                tree(i);
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i=1; i<=n; i++) {
+            if (nodes[i] == 0) {
+                queue.add(i);
+                sb.append(i + " ");
             }
         }
+
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+
+            for (Integer i : list.get(x)) {
+                nodes[i]--;
+                if (nodes[i] == 0) {
+                    queue.add(i);
+                    sb.append(i+ " ");
+                }
+            }
+        }
+
         System.out.println(sb);
-    }
-    public static void tree(int x) {
-        visited[x] = true;
-        sb.append(x).append(" ");
-
-        for (Integer i : list.get(x)) {
-            edge[i]--;
-            if(edge[i] == 0 && !visited[i]) {
-                tree(i);
-            }
-        }
     }
 }
