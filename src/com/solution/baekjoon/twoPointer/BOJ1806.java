ac7c62e2de507133afb29c9827b4cdb15e5f8959
dnd2dnd <tkfdkskarl56@naver.com>
2026-02-05 22:58:08 +0900

    [골드 3] 부분합  - 못풀음

--- src/com/solution/baekjoon/twoPointer/BOJ1806.java
+++ src/com/solution/baekjoon/twoPointer/BOJ1806.java
@@ -5,36 +5,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ1806 {
+public class BOJ1806  {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
         int S = Integer.parseInt(st.nextToken());
 
-        String[] strs = br.readLine().split(" ");
-        int[] nums = new int[N];
-        for (int i=0; i< strs.length; i++) {
-            nums[i] = Integer.parseInt(strs[i]);
+        int[] arr = new int[n];
+        long sum = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        int s = 0, e = 0;
         int answer = Integer.MAX_VALUE;
-
-        int start = 0;
-        int end = 0;
-        int sum = 0;
-        while(start <= end && end < N) {
-            sum += nums[end];
+        while (s<=e && e < n) {
+            sum += arr[e];
 
             while (sum >= S) {
-                answer = Math.min(answer, end-start + 1);
-                sum -= nums[start];
-                start++;
+                answer = Math.min(answer, e-s+1);
+                sum -= arr[s];
+                s++;
             }
-            end++;
+            e++;
         }
-
         if (answer == Integer.MAX_VALUE) {
             System.out.println(0);
         } else {
